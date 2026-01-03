77c09ea4d807588130ae8df544ee41d5ba4a993d
Santapong <santapongsondhi@gmail.com>
2026-01-03 15:08:32 +0700

    Adjust file structure

---
+++ backend/app.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+import os
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+# Get Environment
+
+
+app = FastAPI(
+
+)
+
+
+# Declare Middleware
+
+
+
+# Add Router
+
+
+# Run app
